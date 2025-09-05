47adf345831917acab5c614959ba94ca7dce79aa
Sandro Nolasco <sandronolasco1@gmail.com>
2025-09-05 02:08:47 -0300

    fix: add top edge for ios

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -7,7 +7,7 @@ import { ToastProvider } from '@/components/ui/Toast';
 import config from '@/tamagui.config';
 import { useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import { useColorScheme } from 'react-native';
+import { Platform, useColorScheme } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { TamaguiProvider, Theme, YStack } from 'tamagui';
 import { BrandedLoader } from '../components/BrandedLoader';
@@ -37,7 +37,10 @@ const RootLayout = () => {
               <ErrorBoundary>
                 <ToastProvider>
                   <YStack flex={1} bg="$background">
-                    <SafeAreaView style={{ flex: 1 }} edges={['bottom']}>
+                    <SafeAreaView
+                      style={{ flex: 1 }}
+                      edges={Platform.OS === 'android' ? ['bottom'] : ['bottom', 'top']}
+                    >
                       <Stack
                         screenOptions={{
                           animation: 'none',
